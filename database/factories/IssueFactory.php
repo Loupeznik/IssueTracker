fc6106f773a3d2fdf41b54041554e07b320c184d
Dominik Zarsky <43666286+Loupeznik@users.noreply.github.com>
2020-08-15 15:14:29 +0200

    populate issue factory and seeder

--- database/factories/IssueFactory.php
+++ database/factories/IssueFactory.php
@@ -7,6 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Issue::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => factory(\App\User::class),
+        'Name' => $faker->sentence(),
+        'Desc' => $faker->paragraph(),
+        'types_id' => $faker->numberBetween(1,5),
+        'status_id' => $faker->numberBetween(1,2),
+        'priority_id' => $faker->numberBetween(1,3)
     ];
 });
--- database/seeds/IssueSeeder.php
+++ database/seeds/IssueSeeder.php
@@ -13,13 +13,13 @@ class IssueSeeder extends Seeder
     public function run()
     {
         DB::table('types')->insert([
-            'Name' => ['UI', 'Database', 'Codebase', 'Bug', 'Hardware']
+            'Name' => [1 => 'UI', 2 => 'Database', 3 => 'Codebase', 4 => 'Bug', 5 => 'Hardware']
         ]);
-        DB::table('status')->insert([
-            'Name' => ['Active', 'Resolved']
+        DB::table('statuses')->insert([
+            'Name' => [1 => 'Active', 2 => 'Resolved']
         ]);
-        DB::table('priority')->insert([
-            'Name' => ['High', 'Medium', 'Low']
+        DB::table('priorities')->insert([
+            'Name' => [1 => 'High', 2 => 'Medium', 3 => 'Low']
         ]);
     }
 }
