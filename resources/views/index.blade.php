c970887dec847d160476862f311ccc5407587c6d
Dominik Zarsky <43666286+Loupeznik@users.noreply.github.com>
2020-08-23 09:46:57 +0200

    Add logout form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,9 +8,8 @@
     <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/js/uikit.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/css/uikit.min.css">
     <link rel="stylesheet" href="/main.css">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
     @yield('head')
-
 </head>
 <body>
     <nav class="uk-navbar-container" uk-navbar>
@@ -20,11 +20,15 @@
             <ul class="uk-navbar-nav">
                 <li class="navbar-text"><a href="/issues/list/">All Issues</a></li>
                 @auth
-                <li class="navbar-text"><a href="/issues/create/">New Issue</a></li>
-                <li class="navbar-text"><a href="/account">icon | Logged in as {{Auth::user()->name}}</a></li>
+                    <li class="navbar-text"><a href="/issues/create/">New Issue</a></li>
+                    <li class="navbar-text"><a href="/account">icon | Logged in as {{ Auth::user()->name }}</a></li>
+                    <li class="navbar-text"><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log Out</a></li>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 @endauth
                 @Guest
-                <li class="navbar-text"><a href="/login">Log In</a></li>
+                    <li class="navbar-text"><a href="/login">Log In</a></li>
                 @endguest
             </ul>
         </div>
