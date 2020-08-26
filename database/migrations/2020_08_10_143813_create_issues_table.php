89f65b9f55023a25529af7c0505fa0b2c1e7190b
Dominik Zarsky <43666286+Loupeznik@users.noreply.github.com>
2020-08-26 18:33:31 +0200

    fix foreign keys

--- database/migrations/2020_08_10_143813_create_issues_table.php
+++ database/migrations/2020_08_10_143813_create_issues_table.php
@@ -17,15 +17,15 @@ class CreateIssuesTable extends Migration
             $table->id();
             $table->string('Name',100);
             $table->unsignedBigInteger('types_id');
-            $table->unsignedBigInteger('status_id')->default('1');
+            $table->unsignedBigInteger('status_id')->default(1);
             $table->unsignedBigInteger('priority_id');
             $table->text('Desc', 1000);
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
             $table->softDeletes('deleted_at', 0);
-            $table->foreign('types_id')->references('types.id')->on('types');
-            $table->foreign('status_id')->references('statuses.id')->on('statuses');
-            $table->foreign('priority_id')->references('priorities.id')->on('priorities');
+            $table->foreign('types_id')->references('id')->on('types');
+            $table->foreign('status_id')->references('id')->on('statuses');
+            $table->foreign('priority_id')->references('id')->on('priorities');
             $table->foreign('user_id')->references('user_id')->on('users');
         });
     }
