5bb8da25a55c53a8936c0130e762006c1fc7df68
Dominik Zarsky <43666286+Loupeznik@users.noreply.github.com>
2021-02-27 11:35:14 +0100

    fix form texts

--- resources/views/admin/priorities/new.blade.php
+++ resources/views/admin/priorities/new.blade.php
@@ -4,13 +4,13 @@
     <div class="uk-child-width-expand@s" uk-grid>
         <div>
             <div class="uk-card uk-card-body center-items">
-                <h2>Edit account details</h2>
+                <h2>Add new issue priority category</h2>
                 <form method="POST" action="">
                     @csrf
                     <div class="input-group">
                         <label>Priority name</label><br>
-                        <input type="text" name="name" value="{{old('Name')}}">
-                        @error('name')
+                        <input type="text" name="Name" value="{{old('Name')}}">
+                        @error('Name')
                             <p class="text-warning">{{$errors->first('Name')}}</p>
                         @enderror
                     </div>
--- resources/views/admin/statuses/new.blade.php
+++ resources/views/admin/statuses/new.blade.php
@@ -1,20 +1,20 @@
 @extends('index')
-@section('title','Category Management - Add Priority')
+@section('title','Category Management - Add Status')
 @section('content')
     <div class="uk-child-width-expand@s" uk-grid>
         <div>
             <div class="uk-card uk-card-body center-items">
-                <h2>Edit account details</h2>
+                <h2>Add new issue status category</h2>
                 <form method="POST" action="">
                     @csrf
                     <div class="input-group">
-                        <label>Priority name</label><br>
-                        <input type="text" name="name" value="{{old('Name')}}">
+                        <label>Status name</label><br>
+                        <input type="text" name="Name" value="{{old('Name')}}">
                         @error('name')
                             <p class="text-warning">{{$errors->first('Name')}}</p>
                         @enderror
                     </div>
-                    <input type="submit" value="Add priority">
+                    <input type="submit" value="Add status">
                 </form>
             </div>
         </div>
